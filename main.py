8037939214daa0dbb543577cf29117f9fc0deee9
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-18 14:34:59 -0300

    Modified 'filename' argument help

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from trainer import Trainer
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Train a CNN to identify pupil position.')
-    parser.add_argument('filename', type=str, help='name of the output file')
+    parser.add_argument('filename', type=str, help='name of the output file (use *.h5 for HDF5 format)')
     parser.add_argument('-v', '--verbose', action='count', default=0, help='increase verbosity level')
     parser.add_argument('-l', dest='limit', type=int, default=0, help='limit the number of images to load')
     parser.add_argument('-s', '--show-database', dest='show', action='store_true', default=False,
