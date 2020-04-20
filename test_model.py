0a988c880d5816812389d4b73b1144627f301d49
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-19 23:47:09 -0300

    Minor changes

--- test_model.py
+++ test_model.py
@@ -26,10 +26,10 @@ def main():
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         eyes = cv2.CascadeClassifier('cascades/haarcascade_eye.xml').detectMultiScale(
-            gray_frame, scaleFactor=1.1, minNeighbors=5
+            gray_frame, scaleFactor=1.2, minNeighbors=5
         )
 
-        for (x, y, h, w) in eyes:
+        for (x, y, h, w) in eyes[:2]:
             roi = gray_frame[y: y + h, x: x + w]
 
             height, width = roi.shape
@@ -39,8 +39,9 @@ def main():
             y_ratio = roi.shape[0] / height
             x_ratio = roi.shape[1] / width
 
-            result = model.predict(np.array([roi]))
-            pupil = (int(round(result[0][0] / x_ratio) + x), int(round(result[0][1] / y_ratio) + y))
+            result = model.predict(np.array([roi]))[0]
+
+            pupil = (int(round(result[0] / x_ratio) + x), int(round(result[1] / y_ratio) + y))
 
             # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
             cv2.circle(frame, pupil, 2, (255, 0, 0), 2)
